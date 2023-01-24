e9b283503f1b9fe6e5e70f8d86f5c7aff8dfb38c
Ekaterina <kat.bommon@gmail.com>
2023-01-24 04:35:42 +0300

    character replacement program

---
+++ lectureC#/lecture_3/task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* В тексте надо заменить проблемы на черточки "|" , маленькие "к" заменить на "К", Большие "С" заменить на "с".
+*/
+
+string GetText()
+{
+    Console.Write("Введите текст -> ");
+string getText =Console.ReadLine();
+return getText;
+
+}
+string ReplaceChar(string  value, char oldChar, char newChar)
+{   string result=String.Empty;
+    for(int i=0; i<value.Length;i++)
+    {
+        if (value[i]==oldChar) result += $"{newChar}";
+        else result+=$"{value[i]}";
+    }
+    return result;
+}
+
+string newText =ReplaceChar(GetText(),' ','|');
+newText =ReplaceChar(newText,'к','К');
+newText =ReplaceChar(newText,'С','с');
+Console.WriteLine(newText);
